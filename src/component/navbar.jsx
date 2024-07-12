3ed4503e2afd94cdb26f65f241fbfbfcab03dfc3
ThaiChuT9 <iamtk4i@gmail.com>
2024-07-12 10:17:29 +0700

    Update navbar.jsx

--- src/component/navbar.jsx
+++ src/component/navbar.jsx
@@ -4,7 +4,7 @@ const navbar = () => {
   return (
     <nav className='bg-blue-700 flex p-1 border rounded-md text-white items-center'>
       <h3 className='font-bold pl-2 text-xl'>Aptech Studio</h3>
-      <p className='pl-5 text-sm font-bold'>My clips</p>
+      <p className='pl-5 text-sm font-semibold'>My clips</p>
     </nav>
   )
 }
